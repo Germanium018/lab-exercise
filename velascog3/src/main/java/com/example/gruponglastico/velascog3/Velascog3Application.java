4dfeb577cfad26407f9a21fa6922c72fb9f46652
german <germanolivervelascoiv2@gmail.com>
2025-10-22 09:59:33 +0800

    Initial Spring Boot project structure

---
+++ velascog3/src/main/java/com/example/gruponglastico/velascog3/Velascog3Application.java
@@ -0,0 +1,13 @@
+package com.example.gruponglastico.velascog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Velascog3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Velascog3Application.class, args);
+	}
+
+}
---
+++ velascog3/src/test/java/com/example/gruponglastico/velascog3/Velascog3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gruponglastico.velascog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Velascog3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
